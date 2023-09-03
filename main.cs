4050b657141bf34e77025ecdcd056e0c53f544d9
andrewDev08 <143661891+andrewDev08@users.noreply.github.com>
2023-09-03 23:00:56 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,52 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstLyricsLineOne = "Oh, well imagine / ";
+    string firstLyricsLineTwo = "As I'm pacing the pews in a church corridor / ";
+    string firstLyricsLineThree = "And I can't help but to hear / ";
+    string firstLyricsLineFour = "No, I can't help but to hear an exchanging of words...";
+    
+    Console.WriteLine(firstLyricsLineOne); 
+    Console.WriteLine(firstLyricsLineTwo);
+    Console.WriteLine(firstLyricsLineThree);
+    Console.WriteLine(firstLyricsLineFour); 
+    Console.WriteLine(" ");
+
+    string secondLyricsLineOne = "Oh-oh-oh-oh-oh, ";
+    string secondLyricsLineTwo = "oh-oh-oh-oh, ";
+    string secondLyricsLineThree = "oh-oh-oh / ";
+    string secondLyricsLineFour = "Caught in a bad romance...";
+    
+    Console.WriteLine(secondLyricsLineOne); 
+    Console.WriteLine(secondLyricsLineTwo);
+    Console.WriteLine(secondLyricsLineThree);
+    Console.WriteLine(secondLyricsLineFour); 
+    Console.WriteLine(" ");
+
+    string thirdLyricsLineOne = "Coming out of my cage";
+    string thirdLyricsLineTwo = "And I've been doing just fine";
+    string thirdLyricsLineThree = "Gotta gotta be down";
+    string thirdLyricsLineFour = "Because I want it all";
+    
+    Console.WriteLine(thirdLyricsLineOne); 
+    Console.WriteLine(thirdLyricsLineTwo);
+    Console.WriteLine(thirdLyricsLineThree);
+    Console.WriteLine(thirdLyricsLineFour);  
+    Console.WriteLine(" ");
+
+    string fourthLyricsLineOne = "Now, this is a story all about how";
+    string fourthLyricsLineTwo = "My life got flipped-turned upside down";
+    string fourthLyricsLineThree = "And I'd like to take a minute";
+    string fourthLyricsLineFour = "Just sit right there";
+    string fourthLyricsLineFive = "I'll tell you how I became the prince of a town called Bel-Air";
+    
+
+    Console.WriteLine(fourthLyricsLineOne); 
+    Console.WriteLine(fourthLyricsLineTwo);
+    Console.WriteLine(fourthLyricsLineThree);
+    Console.WriteLine(fourthLyricsLineFour);
+    Console.WriteLine(fourthLyricsLineFive);
+    Console.WriteLine(" ");
+  }
+}
